0f1b480b9385e8eefd5970dbe5d159df293ab7f1
Marc Wäckerlin <marc@waeckerlin.org>
2016-03-07 19:37:33 +0100

    works

--- Dockerfile
+++ Dockerfile
@@ -5,18 +5,25 @@ ENV TERM xterm
 ENV DOMAIN        ""
 ENV ORGANIZATION  ""
 ENV PASSWORD      ""
-ENV DEBUG         0
+ENV DEBUG         1
 
+RUN echo "slapd slapd/password1 password test" | debconf-set-selections
+RUN echo "slapd slapd/password2 password test" | debconf-set-selections
 RUN apt-get update
 RUN apt-get install -y slapd ldap-utils debconf-utils pwgen
 RUN touch /firstrun
+RUN mkdir /ssl
+RUN mkdir /ssl/certs
+RUN mkdir /ssl/private
+RUN chmod o= /ssl/private
+RUN chgrp openldap /ssl/private
 ADD start.sh /start.sh
 CMD /start.sh
 
 EXPOSE 389
 EXPOSE 636
 
-VOLUME /certs
+VOLUME /ssl
 VOLUME /var/backups
 VOLUME /etc/ldap
 VOLUME /var/lib/ldap
--- start.sh
+++ start.sh
@@ -1,4 +1,90 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+function start() {
+    /usr/sbin/slapd -d ${DEBUG} \
+    -h "ldap:/// ldapi:///" \
+    -g openldap -u openldap \
+    -F /etc/ldap/slapd.d
+}
+
+function startbg() {
+    /usr/sbin/slapd -d 0 \
+    -h "ldap:/// ldapi:///" \
+    -g openldap -u openldap \
+    -F /etc/ldap/slapd.d &
+    PID=$!
+    sleep 5
+    while ! pgrep slapd > /dev/null; do sleep 1; done
+}
+
+function stopbg() {
+    kill $PID
+    while pgrep slapd > /dev/null; do sleep 1; done
+}
+
+function checkConfig() {
+    if ! ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -b cn=config dn 2>/dev/null >/dev/null; then
+        echo "Error: cn=config not found" 1>&2
+        exit 1
+    fi
+}
+
+function checkCerts() {
+    echo -n "  check certificates ... "
+    if test -e /ssl/certs/${DOMAIN}-ca.crt \
+         -a -e /ssl/private/${DOMAIN}.key \
+         -a -e /ssl/certs/${DOMAIN}.pem; then
+        echo "found"
+        chmod o= /ssl/private
+        chgrp openldap /ssl/private
+        ldapmodify -Y external -H ldapi:/// > /dev/null <<EOF
+dn: cn=config
+add: olcTLSCACertificateFile
+olcTLSCACertificateFile: /ssl/certs/${DOMAIN}-ca.crt
+-
+add: olcTLSCertificateKeyFile
+olcTLSCertificateKeyFile: /ssl/private/${DOMAIN}.key
+-
+add: olcTLSCertificateFile
+olcTLSCertificateFile: /ssl/certs/${DOMAIN}.pem
+EOF
+    else
+        echo "no"
+        echo "   to activate TLS/SSL, please install:"
+        echo "    - /ssl/certs/${DOMAIN}-ca.crt"
+        echo "    - /ssl/private/${DOMAIN}.key"
+        echo "    - /ssl/certs/${DOMAIN}.pem"
+    fi
+}
+
+function setConfigPWD() {
+    echo "  set cn=config password"
+    ldapmodify -Y external -H ldapi:/// > /dev/null <<EOF
+dn: cn=config
+changetype: modify
+
+dn: olcDatabase={0}config,cn=config
+changetype: modify
+replace: olcRootPW
+olcRootPW: $(slappasswd -s '${PASSWORD}')
+EOF
+}
+
+function reconfigure() {
+    echo "   reconfigure: ${ORGANIZATION} on ${DOMAIN}"
+    debconf-set-selections <<EOF
+slapd slapd/internal/generated_adminpw password ${PASSWORD}
+slapd slapd/internal/adminpw password ${PASSWORD}
+slapd slapd/password1 password ${PASSWORD}
+slapd slapd/password2 password ${PASSWORD}
+slapd shared/organization string ${ORGANIZATION}
+slapd slapd/purge_database boolean false
+slapd slapd/backend select HDB
+slapd slapd/allow_ldap_v2 boolean false
+slapd slapd/domain string ${DOMAIN}
+EOF
+    dpkg-reconfigure -f noninteractive slapd > /dev/null
+}
 
 if test -e /firstrun; then
     if test -z "${DOMAIN}"; then
@@ -11,58 +97,18 @@ if test -e /firstrun; then
     fi
     if test -z "${PASSWORD}"; then
         export PASSWORD=$(pwgen 20 1)
-        echo "Administrator Password: $PASSWORD"
     fi
     echo "Configuration ..."
-    (
-        echo "slapd slapd/internal/generated_adminpw password ${PASSWORD}"
-        echo "slapd slapd/internal/adminpw password ${PASSWORD}"
-        echo "slapd slapd/password1 password ${PASSWORD}"
-        echo "slapd slapd/password2 password ${PASSWORD}"
-        echo "slapd shared/organization string ${ORGANIZATION}"
-        echo "slapd slapd/purge_database boolean false"
-        echo "slapd slapd/backend select HDB"
-        echo "slapd slapd/allow_ldap_v2 boolean false"
-        echo "slapd slapd/domain string ${DOMAIN}"
-    ) | debconf-set-selections
-    dpkg-reconfigure -f noninteractive slapd
-    /usr/sbin/slapd -d ${DEBUG} \
-        -h "ldap:/// ldapi:///" \
-        -g openldap -u openldap \
-        -F /etc/ldap/slapd.d &
-    PID=$!
-    sleep 5
-    (
-        echo "dn: cn=config"
-        echo "changetype: modify"
-        echo
-        echo "dn: olcDatabase={0}config,cn=config"
-        echo "changetype: modify"
-        echo "add: olcRootDN"
-        echo "olcRootDN: cn=admin,cn=config"
-        echo
-        echo "dn: olcDatabase={0}config,cn=config"
-        echo "changetype: modify"
-        echo "add: olcRootPW"
-        echo "olcRootPW: $(slappasswd -s '${PASSWORD}')"
-        echo
-        echo "dn: olcDatabase={0}config,cn=config"
-        echo "changetype: modify"
-        echo "delete: olcAccess"
-        echo "echo "dn: cn=config""
-        #echo "changetype: modify"
-        #echo
-        #echo "dn: olcDatabase={0}config,cn=config"
-        #echo "changetype: modify"
-        #echo "replace: olcRootPW"
-        #echo "olcRootPW: $(slappasswd -s '${PASSWORD}')"
-    )  | ldapmodify -Y external -H ldapi:///
-    kill $PID
+    reconfigure
+    startbg
+    checkConfig
+    setConfigPWD
+    checkCerts
+    stopbg
     rm /firstrun
     echo "Configuration done."
 fi
 echo "starting slapd ..."
-/usr/sbin/slapd -d ${DEBUG} \
-    -h "ldap:/// ldapi:///" \
-    -g openldap -u openldap \
-    -F /etc/ldap/slapd.d
+echo "Administrator Password: $PASSWORD"
+start
+echo "Error: slapd terminated"
