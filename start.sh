1c662f22e50753aa367f84d80e81ade63d3b4b2f
Marc Wäckerlin <marc@waeckerlin.org>
2017-12-15 16:53:05 +0100

    more feedback on startup

--- start.sh
+++ start.sh
@@ -14,6 +14,7 @@ function start() {
 }
 
 function startbg() {
+    echo -n "  starting openldap in background ... "
     fixperm
     /usr/sbin/slapd -d 0 \
     -h "ldap:/// ldapi:///" \
@@ -22,18 +23,24 @@ function startbg() {
     PID=$!
     sleep 5
     while ! pgrep slapd > /dev/null; do sleep 1; done
+    echo "done."
 }
 
 function stopbg() {
+    echo -n "  stopping openldap running in background ... "
     kill $PID
     while pgrep slapd > /dev/null; do sleep 1; done
+    echo "done."
 }
 
 function checkConfig() {
+    echo -n "  checking configuration ... "
     if ! ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -b cn=config dn 2>/dev/null >/dev/null; then
+        echo "error."
         echo "Error: cn=config not found" 1>&2
         exit 1
     fi
+    echo "ready."
 }
 
 function checkCerts() {
@@ -67,17 +74,18 @@ EOF
 }
 
 function setConfigPWD() {
-    echo "  set cn=config password"
+    echo -n "  set cn=config password ... "
     ldapmodify -Y external -H ldapi:/// > /dev/null <<EOF
 dn: olcDatabase={0}config,cn=config
 changetype: modify
 replace: olcRootPW
 olcRootPW: ${PASSWORD}
 EOF
+    echo "done."
 }
 
 function disallowAnonymousBind() {
-    echo "  disallow anonymous bind"
+    echo -n "  disallow anonymous bind ... "
     ldapmodify -Y external -H ldapi:/// > /dev/null <<EOF
 dn: cn=config
 changetype: modify
@@ -89,10 +97,11 @@ changetype: modify
 add: olcRequires
 olcRequires: authc
 EOF
+    echo "done."
 }
 
 function reconfigure() {
-    echo "   reconfigure: ${ORGANIZATION} on ${DOMAIN}"
+    echo -n "   reconfigure: ${ORGANIZATION} on ${DOMAIN} ... "
     debconf-set-selections <<EOF
 slapd slapd/internal/generated_adminpw password ${PASSWORD}
 slapd slapd/internal/adminpw password ${PASSWORD}
@@ -105,6 +114,7 @@ slapd slapd/allow_ldap_v2 boolean false
 slapd slapd/domain string ${DOMAIN}
 EOF
     dpkg-reconfigure -f noninteractive slapd > /dev/null
+    echo "done."
 }
 
 if test -z "${DOMAIN}"; then
